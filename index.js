3b4dbd240c86f3e4a209a3623336d370cc28f7c3
danivanee <danivanee_170@hotmail.com>
2023-02-01 22:42:11 -0600

    se crearon los parametros xAxis, yAxis

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const grid = document.querySelector('.grid')
+const blockWidth = 10
+const blockHeight = 270
 
- function addBlock() {
+ class block {
+   constructor(xAxis, yAxis) {
+      this.bottomLeft = [xAxis,yAxis]
+      this.bottomRight = [xAxis + blockWidth, yAxis]
+      this.topLeft = [xAxis, yAxis + blockHeight]
+      this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+   }
+ }
+
+
+
+
+
+function addBlock() {
     const block = document.createElement('div')
     block.classList.add('block')
     block.style.left = '10px'
