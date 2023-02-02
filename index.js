dc96dded9aed0ed4b5063ef83cb02465761bb6c3
danivanee <danivanee_170@hotmail.com>
2023-02-01 22:23:31 -0600

    se modifico estilo de la funcion addBlock

--- index.js
+++ index.js
@@ -1,30 +1,13 @@
 const grid = document.querySelector('.grid')
 
-const blockWidth = 100
-const blockHeight = 20
-
-class Block {
-    constructor(xAxis, yAxis) {
-        this.bottomLeft = [xAxis, yAxis] 
-        this.bottomLeft = [xAxis, + blockWidth, yAxis]
-        this.topLeft = [xAxis, yAxis + blockHeight]
-        this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
-    }
-}
-
-const blocks = [
-    new Block(10,270)
-]
-
-
-function addBlocks() {
-    for (let i = 0; i < blocks.length; i++) {
+ function addBlock() {
     const block = document.createElement('div')
     block.classList.add('block')
-    block.style.left = blocks[i].bottomLeft[0] + 'px'
-    block.style.bottom = blocks[i].bottomLeft[1] + 'px'    
+    block.style.left = '10px'
+    block.style.bottom = '270px'    
     grid.appendChild(block)
-    }
-} 
+ }   
 
-addBlocks()
+ addBlock()
+    
+    
