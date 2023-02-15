f96b2baf35c4f71eaa81dc2be6a5a6f9c9a7c68f
danivanee <danivanee_170@hotmail.com>
2023-02-14 22:45:13 -0600

    el juego ya elimina los bloques que la pelota toca

--- index.js
+++ index.js
@@ -119,7 +119,9 @@ function checkForCollisions() {
            ((ballCurrentPosition[1] + ballDiameter) > blocks[i].bottomLeft[1] && ballCurrentPosition[1] < blocks[i].topLeft[1])
         ) {
           const allBlocks = Array.from(document.querySelectorAll('.block'))
-          console.log(allBlocks)
+          allBlocks[i].classList.remove('block')
+          blocks.splice(i, 1)
+          changeDirection()
         }
     }
 
